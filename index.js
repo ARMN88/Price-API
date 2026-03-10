bdba52e9752c931ce5e4b35cc31f2c66082a0ce4
ARMN88 <56323614+ARMN88@users.noreply.github.com>
2026-03-10 16:50:33 -0500

    Accord Engine

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
 const Scraper = require('./Scraper.js');
 
-const rafat = '7087013449@vtext.com';
-const ashraf = '7085514450@tmomail.net';
+const abdul = '7089206228@vtext.com';
 
 const scrapers = [
   new Scraper(
-    'Downpipe',
-    'https://www.amsperformance.com/product/infiniti-q60-q50-vr30ddtt-red-alpha-lower-downpipes/',
-    rafat,
-    '.elementor-element-08e529f',
-    '.price bdi'
-  ),
-  new Scraper(
-    'Wing',
-    'https://jaliscoscarbonfiber.com/products/infiniti-q50-q60-m-style-carbon-fiber-spoiler?variant=41071722823880',
-    rafat,
-    '#ProductPrice',
-    '.money'
-  ),
-  new Scraper(
-    'Oil Catch Can',
-    'https://www.z1motorsports.com/z1-products/z1-motorsports/z1-q50-q60-30t-baffled-oil-catch-can-kit-p-23546.html',
-    rafat,
-    '#display_price_bottom',
-    '.sp-newPrice'
-  ),
-  new Scraper(
-    'Coilovers',
-    'https://zzperformance.com/products/zzp-cadillac-ats-coilovers',
-    ashraf,
-    '.product__price',
-    '.visually-hidden'
+    'Engine',
+    'https://jdmengineschicago.com/honda/engines/acura-tsx-k24a-2-4l-dohc-vtec/845/jdm-2004-2008-acura-tsx-k24a3-2-4l-dohc-vtec-rb1-engine-with-6-speed-m-t/',
+    abdul,
+    '.price_title',
+    '.mt0'
   ),
 ];
 
@@ -38,4 +16,4 @@ const handle = setInterval(async () => {
   scrapers.forEach(async (scraper) => {
     await scraper.scrape();
   });
-}, 1000 * 60 * 60);
+}, 1000 * 60);
