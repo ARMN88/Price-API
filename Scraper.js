1268ebb2789ee3a8fe8d217416900bb13440fba2
ARMN88 <56323614+ARMN88@users.noreply.github.com>
2026-03-11 00:50:35 -0500

    Price Fix

--- Scraper.js
+++ Scraper.js
@@ -42,13 +42,11 @@ class Scraper {
     );
 
     if (newPrice < this.price) {
-      if (!isFirst) {
         await transporter.sendMail({
           from: `${this.name} Bot" <${process.env.EMAIL}>`,
           to: this.number,
           text: `${this.name} was $${this.price}, now $${newPrice}!\n${this.url}`,
         });
-      }
       this.price = newPrice;
     }
   }
