088df1558938065fa79bf5cb5c57e80fefef5efe
ARMN88 <56323614+ARMN88@users.noreply.github.com>
2026-03-11 00:20:46 -0500

    Update

--- Scraper.js
+++ Scraper.js
@@ -37,16 +37,16 @@ class Scraper {
       $(container).find(this.selector).text().substring(1)
     );
 
-    if (newPrice < this.price) {
-      if (!isFirst) {
+    // if (newPrice < this.price) {
+      // if (!isFirst) {
         await transporter.sendMail({
           from: `${this.name} Bot" <${process.env.EMAIL}>`,
           to: this.number,
           text: `${this.name} was $${this.price}, now $${newPrice}!\n${this.url}`,
         });
-      }
-      this.price = newPrice;
-    }
+      // }
+      // this.price = newPrice;
+    // }
   }
 }
 
