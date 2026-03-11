8c966668b7a32909b4afafb36472af1ae9372a30
ARMN88 <56323614+ARMN88@users.noreply.github.com>
2026-03-11 00:48:27 -0500

    Text Selector

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const scrapers = [
     'https://jdmengineschicago.com/honda/engines/acura-tsx-k24a-2-4l-dohc-vtec/845/jdm-2004-2008-acura-tsx-k24a3-2-4l-dohc-vtec-rb1-engine-with-6-speed-m-t/',
     abdul,
     'div.price_title',
-    'h4.mt0'
+    'h4.mt-0'
   ),
 ];
 
